39b388c2b494e7c951139ca61a238409074c602a
mifumi323 <mifumi323@tgws.fromc.jp>
2015-03-17 18:30:53 +0900

    いらんコードをコメントアウト。いずれ消す。

--- TGWS_TS/app.ts
+++ TGWS_TS/app.ts
@@ -1,30 +1,30 @@
 ﻿/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
 
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+//class Greeter {
+//    element: HTMLElement;
+//    span: HTMLElement;
+//    timerToken: number;
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+//    constructor(element: HTMLElement) {
+//        this.element = element;
+//        this.element.innerHTML += "The time is: ";
+//        this.span = document.createElement('span');
+//        this.element.appendChild(this.span);
+//        this.span.innerText = new Date().toUTCString();
+//    }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+//    start() {
+//        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+//    }
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
+//    stop() {
+//        clearTimeout(this.timerToken);
+//    }
 
-}
+//}
 
 $(() => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    //var el = document.getElementById('content');
+    //var greeter = new Greeter(el);
+    //greeter.start();
 });
